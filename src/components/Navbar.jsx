685bf1d88f8a38f35abaa5e7d1fad4810a5411ca
Giddymaster <intjgmwas7@gmail.com>
2025-04-12 13:04:21 +0300

    re-designing the navbar buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,93 +2,63 @@ import {
   AppBar,
   Box,
   Toolbar,
-  IconButton,
   Typography,
-  Menu,
   Container,
   Button,
-  MenuItem,
 } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 
-const pages = ["Products", "Pricing", "Blog"];
-
 function Navbar() {
-  const [anchorElNav, setAnchorElNav] = useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
   return (
-    <AppBar position="static" sx={{ backgroundColor: "white", color: "black" }}>
+    <AppBar
+      position="static"
+      sx={{
+        backgroundColor: "white",
+        color: "black",
+        boxShadow: "none",
+        borderBottom: "1px solid #eee",
+      }}
+    >
       <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <img
-            src={logo}
-            alt="logo"
-            style={{ width: "200px", height: "auto" }}
-          />
-
-          {/* Menu icon for small screens */}
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="open menu"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              anchorEl={anchorElNav}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-              transformOrigin={{ vertical: "top", horizontal: "left" }}
-              sx={{ display: { xs: "block", md: "none" } }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
+        <Toolbar sx={{ justifyContent: "space-between", py: 2 }}>
+          <Box display="flex" alignItems="center">
+            <img
+              src={logo}
+              alt="logo"
+              style={{ width: "180px", height: "auto" }}
+            />
           </Box>
 
-          {/* Desktop menu */}
           <Box
             sx={{
-              flexGrow: 1,
               display: { xs: "none", md: "flex" },
-              gap: 2,
-              pl: 3,
+              gap: 4,
+              justifyContent: "center",
+              flexGrow: 1,
             }}
           >
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ color: "black" }}
-              >
-                {page}
-              </Button>
-            ))}
+            <Typography variant="button" sx={{ cursor: "pointer" }} component={Link} to="/login">
+              Read
+            </Typography>
+            <Typography variant="button" sx={{ cursor: "pointer" }} component={Link} to="/login" >
+              Publish
+            </Typography>
+            <Typography variant="button" sx={{ cursor: "pointer" }} component={Link} to="/login">
+              Blogs
+            </Typography>
           </Box>
 
-          {/* Login / Sign Up */}
           <Box sx={{ display: "flex", gap: 1 }}>
             <Button color="inherit" component={Link} to="/login">
               Login
             </Button>
-            <Button variant="outlined" component={Link} to="/signup">
+            <Button
+              variant="outlined"
+              component={Link}
+              to="/signup"
+              sx={{ borderColor: "black", color: "black" }}
+            >
               Sign Up
             </Button>
           </Box>
