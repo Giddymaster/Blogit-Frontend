43ca8b1cd198df11d317df6298362fb98bcfdddd
Giddymaster <intjgmwas7@gmail.com>
2025-04-15 23:06:38 +0300

    fgmdmg

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { Routes, Route } from "react-router-dom";
-import SignUp from "./pages/SignUp";
-import Login from "./pages/Login";
-import Landing from "./pages/Landing";
-import Blogs from "./pages/Blogs";
-import Read from "./pages/Read";
-import WriteBlog from "./pages/WriteBlog";
-import { Publish } from "@mui/icons-material";
+import { Routes, Route } from 'react-router-dom';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import Landing from './pages/Landing';
+import Blogs from './pages/Blogs';
+import WriteBlog from './pages/WriteBlog';
+import BlogEntry from './pages/BlogEntry';
 
 function App() {
   return (
@@ -13,10 +12,9 @@ function App() {
       <Route path="/" element={<Landing />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/read" element={<Read />} />
-      <Route path="/writeblog" element={<WriteBlog />} />
-      <Route path="/publish" element={<Publish />} />
       <Route path="/blogs" element={<Blogs />} />
+      <Route path="/writeblog" element={<WriteBlog />} />
+      <Route path="/blog/:blogId" element={<BlogEntry />} />
     </Routes>
   );
 }
