0109ba7657af6d408570f41b028e62876a4cae68
Giddymaster <intjgmwas7@gmail.com>
2025-04-15 22:35:51 +0300

    blogs listing page

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,55 +1,67 @@
-import { Box, Typography, Card, CardContent, CardMedia, Grid } from "@mui/material";
-import { useEffect, useState } from "react";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import apiUrl from "../utils/apiUrl"; 
+import { Button, Card, CardContent, Typography, Grid } from "@mui/material";
+import apiUrl from "../utils/apiUrl";
+
+const fetchBlogs = async () => {
+  const res = await axios.get(`${apiUrl}/blogs`);
+  return res.data.blogs;
+};
+
+const deleteBlog = async (id) => {
+  await axios.delete(`${apiUrl}/blogs/${id}`);
+};
 
 function Blogs(){
-  const [blogs, setBlogs] = useState([]);
+  const queryClient = useQueryClient();
 
-  useEffect(() => {
-    const fetchBlogs = async () => {
-      try {
-        const res = await axios.get(`${apiUrl}/blogs`);
-        setBlogs(res.data.blogs);
-      } catch (e) {
-        console.log(e, "Failed to fetch blogs");
-      }
-    };
+  const { data: blogs, isLoading } = useQuery({
+    queryKey: ["blogs"],
+    queryFn: fetchBlogs,
+  });
 
-    fetchBlogs();
-  }, []);
+  const mutation = useMutation({
+    mutationFn: deleteBlog,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["blogs"]);
+    },
+  });
+
+  const handleDelete = (id) => {
+    mutation.mutate(id);
+  };
+
+  if (isLoading) return <div>Loading blogs...</div>;
 
   return (
-    <Box sx={{ p: 3 }}>
-      <Typography variant="h4" gutterBottom>
-        My Blogs
-      </Typography>
-      <Grid container spacing={3}>
-        {blogs.map((blog) => (
-          <Grid item xs={12} sm={6} md={4} key={blog.id}>
-            <Card>
-              {blog.featuredImage && (
-                <CardMedia
-                  component="img"
-                  height="200"
-                  image={blog.featuredImage}
-                  alt={blog.title}
-                />
-              )}
-              <CardContent>
-                <Typography variant="h6" component={Link} to={`/posts/${blog.id}`} style={{ textDecoration: "none", color: "inherit" }}>
-                  {blog.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {blog.excerpt.slice(0, 100)}...
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </Box>
+    <Grid container spacing={2}>
+      {blogs.map((blog) => (
+        <Grid item xs={12} md={6} key={blog.id}>
+          <Card>
+            <CardContent>
+              <Typography variant="h5">{blog.title}</Typography>
+              <Typography variant="body2" color="text.secondary">
+                {blog.excerpt}
+              </Typography>
+              <Link to={`/blogs/${blog.id}`}>
+                <Button variant="outlined" size="small" sx={{ mt: 1 }}>
+                  Read More
+                </Button>
+              </Link>
+              <Button
+                variant="contained"
+                color="error"
+                onClick={() => handleDelete(blog.id)}
+                sx={{ mt: 1, ml: 1 }}
+              >
+                Delete
+              </Button>
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
